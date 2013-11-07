6837dabe9493cbefdbb0fd7efa732f9bbefd9f21
Ryan Park <ryan@runscope.com>
2013-11-07 10:30:44 -0800

    Bump version to 0.1.5 for PyPI submission

--- setup.py
+++ setup.py
@@ -8,7 +8,7 @@ with open('LICENSE','r') as f:
 
 setup(
     name         = 'requests-runscope',
-    version      = '0.1.4.1',
+    version      = '0.1.5',
     packages     = [ 'requests_runscope' ],
     requires     = [ 'requests(>=1.0.0)' ],
     provides     = [ 'requests_runscope' ],
